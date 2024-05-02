89dec43fdddeff6b6ae172da20dcef4ee129d28f
Pacahar <s.kirill29@gmail.com>
2024-05-02 17:40:12 +0300

    Add visualisation of data from DB and add function of addition to admin panel

--- src/main/java/com/example/task/controllers/AdminPanelController.java
+++ src/main/java/com/example/task/controllers/AdminPanelController.java
@@ -4,18 +4,27 @@ import com.example.task.dto.SectionDTO;
 import com.example.task.entity.Section;
 import com.example.task.service.SectionsService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@AllArgsConstructor
 public class AdminPanelController {
+
+    private final SectionsService sectionService;
 
     @GetMapping("/admin")
     public static String admin(String[] args) {
         return "admin.html";
+    }
+    @RequestMapping(value = "/admin", method = RequestMethod.POST)
+    public ResponseEntity<Section> create(@RequestParam String name, @RequestParam String description,
+                                          @RequestParam String time, @RequestParam float duration,
+                                          @RequestParam String day, Model model) {
+        SectionDTO dto = new SectionDTO(name, description, time, duration, day);
+        return new ResponseEntity<>(sectionService.create(dto), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/task/controllers/MainController.java
+++ src/main/java/com/example/task/controllers/MainController.java
@@ -1,13 +1,22 @@
 package com.example.task.controllers;
 
+import com.example.task.entity.Section;
+import com.example.task.service.SectionsService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@AllArgsConstructor
 public class MainController {
+    private final SectionsService sectionService;
     @GetMapping("/")
     public String main(Model model) {
+        List<Section> sections = sectionService.readAll();
+        model.addAttribute("sections", sections);
         return"index.html";
     }
 
